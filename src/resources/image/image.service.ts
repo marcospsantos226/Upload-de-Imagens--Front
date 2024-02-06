b8029972c1fc0aad5e26cecf91b800d52a2719c0
marcospsantos226 <marcospsantos226@gmail.com>
2024-02-06 10:55:33 -0300

    Criando a camada de comunicação com a API

--- src/resources/image/image.service.ts
+++ src/resources/image/image.service.ts
@@ -4,7 +4,7 @@ class ImageService{
     baseURL: string = 'http://localhost:8080/v1/images';
 
     async buscar() : Promise<Image[]>{
-        const response = await fetch{this.baseURL};
+        const response = await fetch(this.baseURL);
         return await response.json();
     }
 }
