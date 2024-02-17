23774b489e68721aed6cd9f6d29168a47a0143cf
marcospsantos226 <marcospsantos226@gmail.com>
2024-02-17 12:12:16 -0300

    Formatar ImageCard para exib~ção da data, tamanho e extension

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -2,13 +2,14 @@
 
 interface ImageCardProps {
     nome?: string;
-    tamanho?: string;
+    tamanho?: number;
     dataUpload?: string;
     src?: string;
+    extension?: string;
 }
 
 export const ImageCard: React.FC<ImageCardProps> = ({
-    nome, dataUpload, src, tamanho
+    nome, dataUpload, src, tamanho, extension
 }: ImageCardProps) => {
 
     function download(){
@@ -20,9 +21,22 @@ export const ImageCard: React.FC<ImageCardProps> = ({
             <img src={src} onClick={download} className="h-56 w-full object-cover rounded-t-md" alt="" />
             <div className="card-body p-4">
                 <h5 className="text-xl font-semibold mb-2 text-gray-600">{nome}</h5>
-                <p className="text-gray-600">{tamanho}</p>
+                <p className="text-gray-600">{extension}</p>
+                <p className="text-gray-600">{formatBytes(tamanho)}</p>
                 <p className="text-gray-600">{dataUpload}</p>
             </div>
         </div>
     )
+}
+
+function formatBytes(bytes: number = 0, decimals = 2){
+    if(!+bytes) return '0 Bytes'
+
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ['Bytes', 'KB', 'MB', 'GB']
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+    return `${parseFloat((bytes / Math.pow(k, i).toFixed(dm)))} ${sizes[i]}`
 }
