44590708c2f9b68a516ce16160f4a424fb38b19d
marcospsantos226 <marcospsantos226@gmail.com>
2024-02-28 22:30:17 -0300

    Melhorando o Loading da tela de busca

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -12,12 +12,13 @@ export default function GaleriaPage() {
     const [images, setImages] = useState<Image[]>([])
     const [query, setQuery] = useState<string>('')
     const [extension, setExtension] = useState<string>('')
+    const [loading, setLoading] = useState<boolean>(false)
 
     async function searchImages(){
-        console.log("Valor digitado: ", query)
+        setLoading(true)
         const result = await useService.buscar(query, extension);
         setImages(result);
-        
+        setLoading(false)
     }
 
     function renderImageCard(image: Image){
@@ -57,7 +58,7 @@ export default function GaleriaPage() {
     }
 
     return (
-        <Template>
+        <Template loading={loading}>
             
             <section className="flex flex-col items-center justify-center my-5">
                 <div className="flex space-x-4">
@@ -71,8 +72,8 @@ export default function GaleriaPage() {
                         <option value="JPEG">JPEG</option>
                         <option value="GIF">Gif</option>
                     </select>
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={searchImages}>Search</button>
-                    <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg">Add New</button>
+                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-300" onClick={searchImages}>Search</button>
+                    <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-300">Add New</button>
                 </div>
             </section>
 
